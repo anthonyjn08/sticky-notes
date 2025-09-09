ce97dd7e6f0d8dc8e5b3f09a7112259a14fe84ba
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-09-09 22:23:34 +0100

    Add base.html to notes/templates

--- sticky_notes/notes/urls.py
+++ sticky_notes/notes/urls.py
@@ -3,40 +3,44 @@ from . import views
 
 urlpatterns = [
     # URL pattern for displating a list of all notes
-    path("", views.note_list, name="note_list"), 
+    path("",
+         views.note_list,
+         name="note_list"
+         ),
 
     # URL pattern for displaying details of a specific note
     path("note/<int:pk>/",
          views.note_detail,
-         name="note_detail"),
+         name="note_detail"
+         ),
 
     # URL pattern for creating a new note
     path("note/create/",
          views.note_create,
          name="note_create"
-    ),
+         ),
 
     # URL pattern for updating an existing note
-    path("note/<int:pk>/edit/", 
-        views.note_update,
-        name="note_update"
-    ),
+    path("note/<int:pk>/edit/",
+         views.note_update,
+         name="note_update"
+         ),
 
     # URL pattern for deleting an existing note
-    path("note/<int:pk>/delete/", 
-        views.note_delete,
-        name="note_delete"
-    ),
+    path("note/<int:pk>/delete/",
+         views.note_delete,
+         name="note_delete"
+         ),
 
     # URL pattern to pin/unpin an existing note
-    path('note/<int:pk>/toggle_pin/', 
-        views.toggle_pin,
-        name='toggle_pin'
-    ),
+    path('note/<int:pk>/toggle_pin/',
+         views.toggle_pin,
+         name='toggle_pin'
+         ),
 
     # URL pattern to archive/unarchive an existing note
-    path('note/<int:pk>/toggle_archive/', 
-        views.toggle_archive,
-        name='toggle_archive'
-    ),
+    path('note/<int:pk>/toggle_archive/',
+         views.toggle_archive,
+         name='toggle_archive'
+         ),
 ]
